22348bd1375a5fe82ca4f7f7b5e97bd92258de5e
fuadhasen <fuadhas6634@gmail.com>
2023-10-18 10:05:44 +0300

    shell

---
+++ command1.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * execute_command_or_args - check command
+ * @buffer: user input
+ * @av: program name
+ *
+ * Return: void
+ */
+
+void execute_command_or_args(char *buffer, char *av[])
+{
+	excute_command(buffer, av);
+}
